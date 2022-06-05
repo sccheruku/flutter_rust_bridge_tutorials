1d450260ab128aba8f7a77b628ea8175a12c4b8b
Sai C <saichaitanya88@gmail.com>
2022-06-05 02:12:11 -0400

    part 1

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_rust_bridge_tutorials/bridge_generated_api.dart';
+
+const base = "rust";
+final path = Platform.isWindows ? "$base.dll" : "lib$base.so";
+late final dylib = Platform.isIOS
+    ? DynamicLibrary.process()
+    : Platform.isMacOS
+        ? DynamicLibrary.executable()
+        : DynamicLibrary.open(path);
+late final api = RustImpl(dylib);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<int> counter;
+
+  @override
+  void initState() {
+    counter = api.getCounter();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      counter = api.increment();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            FutureBuilder<List<dynamic>>(
+                future: Future.wait([counter]),
+                builder: (context, snap) {
+                  final data = snap.data;
+                  if (data == null) {
+                    return const Text("Loading");
+                  }
+                  return Text(
+                    '${data[0]}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
